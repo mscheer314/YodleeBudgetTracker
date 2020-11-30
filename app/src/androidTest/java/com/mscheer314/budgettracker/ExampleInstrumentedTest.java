7a752591f0c81bfdb98f643d023e586913c8b323
Mark Scheer <mscheer314@gmail.com>
2020-11-30 11:54:55 -0600

    Update App Name

--- app/src/androidTest/java/com/mscheer314/budgettracker/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/mscheer314/budgettracker/ExampleInstrumentedTest.java
@@ -22,6 +22,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
 
-        assertEquals("com.yodlee.fastlink", appContext.getPackageName());
+        assertEquals("com.mscheer314.budgettracker", appContext.getPackageName());
     }
 }
